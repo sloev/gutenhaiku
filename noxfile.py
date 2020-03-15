8dc4eea601a8cee1301fc7671a9d6734136b7cdf
sloev <johanned.valbjorn@gmail.com>
2020-03-15 11:37:38 +0100

    travis 2

--- noxfile.py
+++ noxfile.py
@@ -14,12 +14,10 @@ nox.options.sessions = [
 @nox.session(python=False)
 def install_pyenv_versions(session):
     session.run("pyenv", "install", "-s", "3.7.5", external=True, silent=True)
-    session.run("pyenv", "install", "-s", "3.8.0", external=True, silent=True)
 
 
 @nox.session(python=["3.7"], reuse_venv=True)
 def test(session):
-    session.run("poetry", "install", external=True, silent=True)
     session.run("pytest", "tests", silent=True)
 
 
